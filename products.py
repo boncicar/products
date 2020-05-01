eaa78172ba73da7628552f006bd08daaa7903995
WHY <boncicar@gmail.com>
2020-05-01 18:15:54 +0900

    4th version

--- products.py
+++ products.py
@@ -6,8 +6,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')  # 此行寫在if外以及if之後是避免輸入q要跳出程式時會出現請輸入商品價格
-	x = [name, price]     # 建立第二個清單x(二維清單)，直接把name跟price裝進x內
-	products.append(x)    # 把x清單裝入products清單內
+	products.append([name, price])   # 直接把小清單x裝入products清單內，程式更簡潔
 print(products)
 
 # produts[1][0]:1表示products大清單內的第1位置，0表示在products第1位置內的x清單的第0位置
