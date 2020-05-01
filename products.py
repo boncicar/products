c884503c5c5e4c672387af234e027a63a2a6f22d
WHY <boncicar@gmail.com>
2020-05-01 18:17:19 +0900

    5th version

--- products.py
+++ products.py
@@ -8,5 +8,5 @@ while True:
 	price = input('請輸入商品價格: ')  # 此行寫在if外以及if之後是避免輸入q要跳出程式時會出現請輸入商品價格
 	products.append([name, price])   # 直接把小清單x裝入products清單內，程式更簡潔
 print(products)
-
-# produts[1][0]:1表示products大清單內的第1位置，0表示在products第1位置內的x清單的第0位置
+for x in products:  # 以product來讀取products清單中的每一筆資料
+	print(x)        # 印出products清單中的每一筆資料，如果是print(product[0])則會印出每一筆的商品名稱
