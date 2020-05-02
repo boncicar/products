485b9fd00ed11af013881886328544ec6c2e6121
boncicar <64126030+boncicar@users.noreply.github.com>
2020-05-02 18:39:21 +0900

    Update products.py

--- products.py
+++ products.py
@@ -8,5 +8,5 @@ while True:
 	price = input('請輸入商品價格: ')  # 此行寫在if外以及if之後是避免輸入q要跳出程式時會出現請輸入商品價格
 	products.append([name, price])   # 直接把小清單x裝入products清單內，程式更簡潔
 print(products)
-for x in products:  # 以product來讀取products清單中的每一筆資料
+for x in products:  # 以x來讀取products清單中的每一筆資料
 	print(x[0], '的價格是', x[1], '元')        # 印出products清單中的每一筆資料: 商品名稱 + 的價格是+ 價格 + 元
